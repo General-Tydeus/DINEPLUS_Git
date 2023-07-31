7e6ab6d6a1ce6adca42611c61396160118a872cc
Julmar <julmae8@gmail.com>
2023-07-31 13:41:40 +0800

    julmar

--- DINEPLUSWEBAPI/DINEPLUSWEBAPI/FldrClass/ClsGetConnection.cs
+++ DINEPLUSWEBAPI/DINEPLUSWEBAPI/FldrClass/ClsGetConnection.cs
@@ -9,7 +9,7 @@ namespace DINEPLUSWEBAPI.FldrClass
     {
         public string PlsConnect()
         {
-            return "Server = DESKTOP-FHUL9S2\\SQLEXPRESS; Database = DINEPLUS_BE; User ID = server2008; Password = Mssqlone1; Trusted_Connection = False; TrustServerCertificate=True;";
+            return "Server = DESKTOP-N1P7VB9\\SQLEXPRESS; Database = DINEPLUS_BE; User ID = server2008; Password = Mssqlone1; Trusted_Connection = False; TrustServerCertificate=True;";
 
 
 
