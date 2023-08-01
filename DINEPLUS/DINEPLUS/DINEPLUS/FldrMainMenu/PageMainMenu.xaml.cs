ee1893a9638ab9d6c636a0828a4aa30d4db295af
General-Tydeus <jovier.aposaga@gmail.com>
2023-08-01 16:07:10 +0800

    1

--- DINEPLUS/DINEPLUS/DINEPLUS/FldrMainMenu/PageMainMenu.xaml.cs
+++ DINEPLUS/DINEPLUS/DINEPLUS/FldrMainMenu/PageMainMenu.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
@@ -15,19 +15,71 @@ namespace DINEPLUS.FldrMainMenu
     public partial class PageMainMenu : Xamarin.Forms.TabbedPage
     {
         public static PageMainMenu Instance;
-
+        private string strLoggedIn;
         public PageMainMenu()
         {
             InitializeComponent();
              On<Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
-            Errors();
+            //Children.Add(new PagePAYO()); // First tab
+            //Children.Add(new PageSO()); // Second tab
             Instance = this;
+           // CurrentPageChanged += OnTabChanged;
+            OnOpeningPage();
         }
-        public void Errors()
-        {
-            //await DisplayAlert("USERCODE", PageLogin.glbltxtUserCode, "Oks");
-            //await DisplayAlert("CNCODE", PageLogin.glbltxtCNCode, "Oks");
 
+        public void DisableAllTabs()
+        {
+            //Xamarin.Forms.TabbedPage.IsEnabled = false;
+
+            // DisplayAlert("Alert!", "Disable Tab Pages", "Yes", "No");
+        }
+
+        public void EnableAllTabs()
+        {
+            foreach (var tab in Children)
+            {
+                if (tab is ContentPage contentPage)
+                {
+                    contentPage.IsEnabled = true;
+                }
+            }
+        }
+        private async void OnOpeningPage()
+        {
+            strLoggedIn = Preferences.Get("LogCheck", "1");//kon get ma kuha kon wala t ee set ya new, kon set t set ee
+            if (strLoggedIn == "1") //one proceed sa login 
+            {
+                await Navigation.PushAsync(new PageLogin());
+                return;
+            }
+            else if (string.IsNullOrEmpty(strLoggedIn))
+            {
+                await Navigation.PushAsync(new PageLogin());
+                return;
+            }
+        }
+
+        private void btnLogout_Clicked(object sender, EventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await this.DisplayAlert("Alert!", "Do you want to LogOut?", "Yes", "No");
+                if (result == true)
+                {
+                    Preferences.Clear();
+                    Preferences.Set("LogCheck", "1"); // one == login page
+                    await Task.Delay(500);
+                    await Navigation.PushAsync(new PageLogin());
+                    var currenPage = Navigation.NavigationStack[Navigation.NavigationStack.Count - 1];
+                    var pageList = Navigation.NavigationStack.Where(y => y != currenPage).ToList();
+                    foreach (var page in pageList)
+                        Navigation.RemovePage(page);
+                }
+                else
+                {
+                    await Xamarin.Forms.Application.Current.SavePropertiesAsync();
+                }
+            });
         }
     }
 }
---
+++ DINEPLUS/DINEPLUS/DINEPLUS/FldrPopup/PopUpdOrd.xaml.cs
@@ -0,0 +1,58 @@
+﻿using Rg.Plugins.Popup.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DINEPLUS.FldrPopup
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PopUpdOrd : Rg.Plugins.Popup.Pages.PopupPage
+    {
+        public PopUpdOrd()
+        {
+            InitializeComponent();
+            lblPrice.Text = PageViewOrders.Instance.varSP.ToString("n2");
+            lblTotal.Text = PageViewOrders.Instance.varSP.ToString("n2");
+            txtQty.Text = PageViewOrders.Instance.varQty.ToString();
+        }
+
+        private void txtQty_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtQty.Text == "")
+            {
+                lblTotal.Text = "0";
+            }
+            else
+            {
+                lblTotal.Text = (double.Parse(txtQty.Text) * PageViewOrders.Instance.varSP).ToString("N2");
+            }
+        }
+
+        private void stepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            double value = e.NewValue;
+            lblTotal.Text = tPrice(PageViewOrders.Instance.varSP, value).ToString("N2");
+        }
+        public double tPrice(double qty, double prices)
+        {
+            return qty * prices;
+        }
+        private async void btnSave_Clicked(object sender, EventArgs e)
+        {
+            if (int.Parse(txtQty.Text) <= 0)
+            {
+                txtQty.Focus();
+                await this.DisplayToastAsync("Invalid Quantity");
+                return;
+            }
+
+            PageViewOrders.Instance.AddOrd(txtQty.Text);
+            await Navigation.PopPopupAsync();
+        }
+    }
+}
